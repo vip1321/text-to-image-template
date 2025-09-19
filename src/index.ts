3eab61446f154c3c144256a91c31c1a00cc83455
FB刷粉平台 <75359667+vip1321@users.noreply.github.com>
2025-09-20 01:49:16 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "social media",
+      prompt: "social media cat",
     };
 
     const response = await env.AI.run(
