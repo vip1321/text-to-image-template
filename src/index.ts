6333a19c7d1587655fe10016e9b37a063f2bfdf9
FB刷粉平台 <75359667+vip1321@users.noreply.github.com>
2025-09-20 01:47:10 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "Chinese style girl social media",
+      prompt: "social media",
     };
 
     const response = await env.AI.run(
